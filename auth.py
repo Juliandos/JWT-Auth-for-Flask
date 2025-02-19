724068433c9999be24d188ce86a19706ae9d92d9
Juliandos <95juliandos@gmail.com>
2025-02-19 08:39:43 -0600

    crear access token y refresh token

--- auth.py
+++ auth.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, jsonify, request
-# from flask_jwt_extended import (
-#     create_access_token,
-#     create_refresh_token,
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
 #     jwt_required,
 #     get_jwt,
 #     current_user,
 #     get_jwt_identity,
-# )
+)
 from models import User
 
 auth_bp = Blueprint("auth", __name__)
@@ -30,27 +30,27 @@ def register_user():
     return jsonify({"message": "User created"}), 201
 
 
-# @auth_bp.post("/login")
-# def login_user():
-#     data = request.get_json()
+@auth_bp.post("/login")
+def login_user():
+    data = request.get_json()
 
-#     user = User.get_user_by_username(username=data.get("username"))
+    user = User.get_user_by_username(username=data.get("username"))
 
-#     if user and (user.check_password(password=data.get("password"))):
-#         access_token = create_access_token(identity=user.username)
-#         refresh_token = create_refresh_token(identity=user.username)
+    if user and (user.check_password(password=data.get("password"))):
+        access_token = create_access_token(identity=user.username)
+        refresh_token = create_refresh_token(identity=user.username)
 
-#         return (
-#             jsonify(
-#                 {
-#                     "message": "Logged In ",
-#                     "tokens": {"access": access_token, "refresh": refresh_token},
-#                 }
-#             ),
-#             200,
-#         )
+        return (
+            jsonify(
+                {
+                    "message": "Logged In ",
+                    "tokens": {"access": access_token, "refresh": refresh_token},
+                }
+            ),
+            200,
+        )
 
-#     return jsonify({"error": "Invalid username or password"}), 400
+    return jsonify({"error": "Invalid username or password"}), 400
 
 
 # @auth_bp.get("/whoami")
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from extensions import db, jwt
 from auth import auth_bp
-# from users import user_bp
+from users import user_bp
 # from models import User, TokenBlocklist
 
 
@@ -12,11 +12,11 @@ def create_app():
 
     # initialize exts
     db.init_app(app)
-    # jwt.init_app(app)
+    jwt.init_app(app)
 
     # # register bluepints
     app.register_blueprint(auth_bp, url_prefix="/auth")
-    # app.register_blueprint(user_bp, url_prefix="/users")
+    app.register_blueprint(user_bp, url_prefix="/users")
 
     # # load user
     # @jwt.user_lookup_loader
